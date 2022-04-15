facc8b600df49c38c9ad2768c4cef7241b303668
Jeremy Moser <jeremygmoser@icloud.com>
2022-04-15 11:03:28 -0400

    Further cosmetic changes to the semester summary output.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Jeremy G. Moser - jeremygmoser@icloud.com
-# Wednesday, March 30, 2022
+# Thursday, April 7, 2022
 # GPA Calculator
 
 numClasses = input("Enter the number of classes you are taking this semester: ")
@@ -9,7 +9,7 @@ x = 0
 while x < int(numClasses):
     print("\nCourse #" + str(x + 1) + " Details:")
     courseName = input(">> Enter course name: ")
-    courseCreditHours = input(">> Enter course credit hour(s): ")
+    courseCreditHours = float(input(">> Enter course credit hour(s): "))
     courseLetterGrade = input(">> Enter course letter grade: ")
     semesterGPA.append([courseName, courseLetterGrade, courseCreditHours])
     x += 1
@@ -18,12 +18,13 @@ c = 1
 cumGradeValues = 0
 cumCreditHours = 0
 cumGradePoints = 0
-print("\n***** SEMESTER SUMMARY *****")
+
+print("\n{:*^117}".format(str("  SEMESTER SUMMARY  ")))
 print("\n{:10}".format(str("Course #")) + "{:50}".format(str("Course Name"))
-    + "{:10}".format(str("Grade")) + "{:15}".format(str("Grade Value"))
-    + "{:12}".format(str("Credit(s)")) + "{:10}".format(str("Point(s)"))
-    + "{:5}".format(str("GPA")))
-print(str("----------------------------------------------------------------------------------------------------------------"))
+    + "{:>10}".format(str("Grade")) + "{:>15}".format(str("Grade Value"))
+    + "{:>13}".format(str("Credit(s)")) + "{:>12}".format(str("Point(s)"))
+    + "{:>7}".format(str("GPA")))
+print("{:-<117}".format(str("")))
 
 while c < len(semesterGPA):
     courseName = semesterGPA[c][0]
@@ -31,30 +32,33 @@ while c < len(semesterGPA):
     courseCreditHours = semesterGPA[c][2]
     courseGradeValue = 0
     if courseLetterGrade.upper() == "A":
-        courseGradeValue = 4
+        courseGradeValue = 4.0
     elif courseLetterGrade.upper() == "B":
-        courseGradeValue = 3
+        courseGradeValue = 3.0
     elif courseLetterGrade.upper() == "C":
-        courseLetterValue = 2
+        courseGradeValue = 2.0
     elif courseLetterGrade.upper() == "D":
-        courseGradeValue = 1
+        courseGradeValue = 1.0
     elif courseLetterGrade.upper() == "F":
-        courseGradeValue = 0
+        courseGradeValue = 0.0
     else:
-        courseGradeValue = 0
-    courseGPA = (int(courseCreditHours) * int(courseGradeValue)) / int(courseCreditHours)
-    cumGradeValues += int(courseGradeValue)
-    cumCreditHours += int(courseCreditHours)
-    cumGradePoints += int(courseCreditHours) * int(courseGradeValue)
+        courseGradeValue = 0.0
+    courseGPA = (float(courseCreditHours) * float(courseGradeValue)) / float(courseCreditHours)
+    cumGradeValues += float(courseGradeValue)
+    cumCreditHours += float(courseCreditHours)
+    cumGradePoints += float(courseCreditHours) * float(courseGradeValue)
 
     print("{:10}".format("#" + str(c) + ":") + "{:50}".format(str(courseName))
-          + "{:10}".format(str(courseLetterGrade)) + "{:15}".format(str(courseGradeValue))
-          + "{:12}".format(str(courseCreditHours)) + "{:10}".format(str(int(courseGradeValue) * int(courseCreditHours)))
-          + "{:>5}".format(str(courseGPA)))
-    print(str("----------------------------------------------------------------------------------------------------------------"))
+          + "{:>10}".format(str(courseLetterGrade)) + "{:>15}".format(str(courseGradeValue))
+          + "{:>13}".format(str(courseCreditHours)) + "{:>12}".format(str(float(courseGradeValue) * float(courseCreditHours)))
+          + "{:>7}".format(str(courseGPA)))
+    print("{:-<117}".format(str("")))
     c += 1
 
-print("{:60}".format(str("Total Course(s): " + str(numClasses))) + "{:10}".format("")
-    + "{:15}".format(str(cumGradeValues))
-    + "{:12}".format(str(cumCreditHours)) + "{:10}".format(str(cumGradePoints))
-    + "{:>5}".format(str(int(cumGradePoints) / int(cumCreditHours))))
+print("{:60}".format(str("Total Course(s): " + str(numClasses))) + "{:>10}".format("")
+    + "{:>15}".format(str(cumGradeValues))
+    + "{:>13}".format(str(cumCreditHours)) + "{:>12}".format(str(cumGradePoints))
+    + "{:>7}".format(str(float(cumGradePoints) / float(cumCreditHours))))
+print("{:=<117}".format(str("")))
+print()
+print("{:*<117}".format(str("")))
